5a2b469f5f70bf74960a1ff25376c5b614676774
Nguyen Long <nguyenlong232253@gmail.com>
2022-07-21 14:49:32 +0700

    thanh

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -13,7 +13,7 @@ export class AuthService {
 // API LOCAL
   private API_SIGNUP = environment.API_LOCAL + 'signup';
   private API_SIGNIN = environment.API_LOCAL + 'signin';
-  // private API_CHANGE_AVATAR = environment.API_LOCAL + 'change-avatar';
+  private API_CHANGE_AVATAR = environment.API_LOCAL + 'change-avatar';
 
   // API SERVER
   //@ts-ignore
@@ -31,9 +31,9 @@ export class AuthService {
   signIn(signIn: SignInForm): Observable<JwtResponse>{
     return this.http.post<JwtResponse>(this.API_SIGNIN, signIn);
   }
-  // changeAvatar(info: any): Observable<JwtResponse>{
-  //   return this.http.put<JwtResponse>(this.API_CHANGE_AVATAR, info);
-  // }
+  changeAvatar(info: any): Observable<JwtResponse>{
+    return this.http.put<JwtResponse>(this.API_CHANGE_AVATAR, info);
+  }
 
   setData(data: boolean){
     this.data = data;

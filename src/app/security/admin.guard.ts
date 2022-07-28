6e9a9b487800e97289779555962795851d7463a6
Nguyen Long <nguyenlong232253@gmail.com>
2022-07-28 12:41:53 +0700

    luu

--- src/app/security/admin.guard.ts
+++ src/app/security/admin.guard.ts
@@ -18,17 +18,6 @@ export class AdminGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    // if(this.tokenService.getToken()){
-    //   if(JSON.stringify(this.tokenService.getRole())==JSON.stringify(this.admin)){
-    //     console.log('Goi dung la ADMIN');
-    //     return true;
-    //   }else {
-    //     console.log('**** Goi khong dung ADMIN');
-    //     this.router.navigate(['/'])
-    //   }
-    // } else {
-    //   this.router.navigate(['/login'])
-    // }
     return this.checkAdmin();
   }
   checkAdmin(){

78353f4072bb00569193b139b1fdf4ab5a0b598e
Nguyen Long <nguyenlong232253@gmail.com>
2022-07-28 12:14:43 +0700

    phan trang admin-user, phan quyen truy cap Guard

--- src/app/security/admin.guard.ts
+++ src/app/security/admin.guard.ts
@@ -1,13 +1,14 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import {AuthService} from "../service/auth.service";
 import {TokenService} from "../service/token.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AdminGuard implements CanActivate {
+  admin: any = ["ADMIN"]
   constructor(private authService: AuthService,
               private tokenService: TokenService,
               private router: Router
@@ -17,23 +18,25 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.checkUser();
+    // if(this.tokenService.getToken()){
+    //   if(JSON.stringify(this.tokenService.getRole())==JSON.stringify(this.admin)){
+    //     console.log('Goi dung la ADMIN');
+    //     return true;
+    //   }else {
+    //     console.log('**** Goi khong dung ADMIN');
+    //     this.router.navigate(['/'])
+    //   }
+    // } else {
+    //   this.router.navigate(['/login'])
+    // }
+    return this.checkAdmin();
   }
-
-  checkUser() {
-    if (this.tokenService.getRole()[0] == "USER" || "ADMIN") {
+  checkAdmin(){
+    if (this.tokenService.getRole()[0] == "ADMIN") {
       return true;
     } else {
       this.router.navigate([''])
       return false;
     }
-    //
-    // if (this.tokenService.getRole()[0] == "ADMIN"){
-    //   return true;
-    // }else {
-    //   this.router.navigate([''])
-    //   return false;
-    // }
   }
-
 }
